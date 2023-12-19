1caffdb73b047af4625987d183d9c6598263a52d
dipankarad <developerdipankar10@gmail.com>
2023-12-19 15:40:35 +0530

    added 15:40

---
+++ map2.js
@@ -0,0 +1,14 @@
+const student=[
+    {id:101 , name:"Sony"},
+    {id:102 , name:"Katsumi"},
+    {id:103 , name:"Jeje"}
+]
+
+const names=student.map(s=>s.name);
+console.log(names);
+const ids=student.map(s=>s.id);
+console.log(ids);
+const bigger=student.find(s=>s.id>101);
+const biggerOne=student.filter(s=>s.id>102);
+console.log(bigger)
+console.log(biggerOne)
